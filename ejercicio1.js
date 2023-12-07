dd5f7a2f566c32ba21bf71b4a7953b1cef874d61
MargaritaAlvarezGonz <margarita.alvarez3105@gmail.com>
2023-12-07 05:18:35 -0600

    javascript pruebas

---
+++ ejercicio1.js
@@ -0,0 +1,8 @@
+/*Enuncuado Ejercicio 1:  
+1. Hacer un array de aceites esenciales y hacer un programa que pueda mostrar
+la información de cada aceite esencial. 
+2. Añadir la capacidad de buscar información de varios aceites esenciales a la vez*/
+
+// Crear un objeto con las caracteristicas
+//Mostrarlo en consola
+
